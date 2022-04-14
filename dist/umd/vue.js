114ab059d9d69fc02a986e5743202aa98a369c70
guonan <838093891@qq.com>
2022-04-14 14:51:16 +0800

    chore readme

--- dist/umd/vue.js
+++ dist/umd/vue.js
@@ -43,7 +43,12 @@
     return obj !== null && _typeof(obj) === "object";
   }
 
-  impo;
+  // 需要重写数组中的7个方法， push pop shift unshift splice sort reverse
+  // 会导致数组的长度发生变化，需要通知观察者
+  var arrayProto = Array.prototype; // value.__proto__ = arrayMethods 将数组的原型指向arrayMethods，这样就可以访问到数组中我们重写的方法 。
+  // arrayMethods.__proto__ = arrayProto 将arrayMethods的原型指向数组的原型
+
+  var arrayMethods = Object.create(arrayProto);
 
   var Observer = /*#__PURE__*/function () {
     function Observer(value) {
